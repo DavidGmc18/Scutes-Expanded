97914dc4a72880bbb7e7f8936dba8804a1bb5758
cinchtail <cinchtail@gmail.com>
2024-08-05 14:00:00 -0400

    some of the block stuff added

--- fabric/src/main/java/net/scutes_expanded/ScutesExpanded.java
+++ fabric/src/main/java/net/scutes_expanded/ScutesExpanded.java
@@ -1,13 +1,13 @@
 package net.scutes_expanded;
 
 import net.fabricmc.api.ModInitializer;
-import net.scutes_expanded.CommonClass;
-import net.scutes_expanded.Constants;
+import net.scutes_expanded.block.ModBlocks;
 
 public class ScutesExpanded implements ModInitializer {
-    
+
     @Override
     public void onInitialize() {
         CommonClass.init();
+        ModBlocks.registerModBlocks();
     }
 }
---
+++ fabric/src/main/java/net/scutes_expanded/block/ModBlocks.java
@@ -0,0 +1,32 @@
+package net.scutes_expanded.block;
+
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.scutes_expanded.Constants;
+
+public class ModBlocks {
+
+    public static final Block TURTLE_SCUTE_BLOCK = register("turtle_scute_block",
+            new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_GREEN).requiresCorrectToolForDrops().strength(20.0F, 200.0F)));
+    public static final Block ARMADILLO_SCUTE_BLOCK = register("armadillo_scute_block",
+            new Block(BlockBehaviour.Properties.of().mapColor(MapColor.COLOR_BROWN).requiresCorrectToolForDrops().strength(20.0F, 200.0F)));
+
+    private static Block register(String name, Block block) {
+        registerBlockItem(name, block);
+        return Registry.register(BuiltInRegistries.BLOCK, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID , name), block);
+    }
+
+    private static Item registerBlockItem(String name, Block block) {
+        return Registry.register(BuiltInRegistries.ITEM, ResourceLocation.fromNamespaceAndPath(Constants.MOD_ID , name),
+                new BlockItem(block, new Item.Properties()));
+    }
+    public static void registerModBlocks() {
+        Constants.LOG.info("Registering ModBlocks for " + Constants.MOD_ID);
+    }
+}
